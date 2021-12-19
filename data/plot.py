3df60885a4c5878d1f478377ad6bcb45293a8bda
Erik Koiv <koiverik@gmail.com>
2021-12-19 02:19:25 +0200

    Init

---
+++ data/plot.py
@@ -0,0 +1,48 @@
+import numpy as np 
+import pandas as pd 
+from scipy.signal import savgol_filter
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+sns.set_theme()
+
+circles = pd.read_csv("nonHolonomicSq.csv", usecols=[1, 2, 3, 4, 5, 6])
+
+# Time 0 - 300 (5 mins)
+t = np.linspace(0, 300, 1499)
+
+# Current Draw - A
+c = circles["current"][1:]
+c_f = savgol_filter(c, 99, 3)
+
+# Battery %
+b = circles["batteryPercentage"][1:]
+b_f = savgol_filter(b, 99, 3)
+
+# Capacity % - remaining / max
+r = circles["remainingCapacity"][1:] / circles["maxCapacity"][1:]
+
+fig, axs = plt.subplots(2, figsize=(18, 25), dpi=80)
+fig.suptitle("Non-Holonomic Sq Path", fontsize=30)
+
+axs[0].plot(t, c, label= "Current Draw (A)", color="blue", linestyle="dotted", linewidth=1.5)
+axs[0].plot(t, c_f, label = "Savitzky-Golay Filtered Draw (A)", color="red", linewidth=2)
+
+axs[1].plot(t, b, label="Battery Voltage Based (%)", color="orange", linestyle="dashdot")
+axs[1].plot(t, b_f, label = "Savitzky-Golay Filtered Battery (%)", color="red", linewidth=2)
+axs[1].plot(t, r * 100, label="Capacity Based (%)", color="green", linestyle="dashdot", linewidth=3.0)
+
+axs[0].set_ylabel("Current Draw (A)", fontsize=25)
+axs[1].set_ylabel("Remaining Battery (%)", fontsize=25)
+
+axs[1].set_xlabel("Time (s)", fontsize=25)
+
+axs[0].set_xlim(0, 300)
+axs[1].set_xlim(0, 300)
+
+axs[0].set_ylim(0, 20)
+axs[1].set_ylim(0, 52)
+
+axs[0].legend(loc="upper right", prop={"size": 15}, shadow=True, fancybox=True)
+axs[1].legend(loc="lower right", prop={"size": 15}, shadow=True, fancybox=True)
+
+plt.show()
---
+++ scripts/processing.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import rospy
+import pandas as pd
+import os
+import uuid
+from robotont_msgs.msg import PowerSupply
+from statistics import mean
+
+class processor:
+    def __init__(self):
+        self.firstRun = True
+        self.powerData = {
+            "time": rospy.Time.now().to_sec(),
+            #It is expected that the max capacity is measured from 16.8V to nominal voltage of 14.8
+            "maxCapacity": 5.2 * 3.6 * pow(10,3),
+            "startingCapacity": 0.0,
+            "current": 0.0,
+            "startingCapacity": 0.0,
+            "current": 0.0,
+            "batteryPercentage": 0.0,
+            "remainingCapacity": 0.0
+        }
+        self.startingTime = self.powerData["time"]
+        self.df = pd.DataFrame(self.powerData, index=[0])
+        self.sub()
+        self.batRange = 2
+        self.batMax = 16.8
+        self.batMin = 14.8
+        self.Varray = []
+        self.Aarray = []
+        self.path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        rospy.logwarn(self.path)
+
+
+
+    def callback(self, data):
+        if len(self.Varray) < 9 or len(self.Aarray) < 9:
+            self.Aarray.append(data.current)
+            self.Varray.append(data.voltage)
+        else:
+            #append last time so amount of measurements is 10
+            self.Aarray.append(data.current)
+            self.Varray.append(data.voltage)
+
+            V = mean(self.Varray)
+            A = mean(self.Aarray)
+
+            #calculate battery percentage
+            if self.batMax - V >= 2:
+                self.powerData["batteryPercentage"] = 0.0
+            else:
+                self.powerData["batteryPercentage"] =  round(((V - self.batMin)/self.batRange)*100)
+
+            #if first run set some values
+            if self.firstRun:
+                self.powerData["startingCapacity"] = self.powerData["maxCapacity"] * (self.powerData["batteryPercentage"]/100)
+                self.powerData["remainingCapacity"] = self.powerData["startingCapacity"]
+                self.powerData["current"] = A
+                self.firstRun = False
+
+            previousCurrent = self.powerData["current"]
+            self.powerData["current"] = A
+            
+            previousTime = self.powerData["time"]
+            self.powerData["time"] = rospy.Time.now().to_sec()
+            elapsed = self.powerData["time"] - previousTime
+            consumed = mean((self.powerData["current"], previousCurrent))*elapsed
+            
+            self.powerData["remainingCapacity"] = self.powerData["remainingCapacity"] - consumed
+            self.df = self.df.append(self.powerData, ignore_index=True)
+            rospy.loginfo("Testing for {} more seconds".format(300 - (self.powerData["time"] - self.startingTime)))
+
+            #reset the measurement arrays
+            self.Aarray = []
+            self.Varray = []
+
+        if self.powerData["time"] - self.startingTime >= 300:
+            self.shutDown()
+
+
+
+    def sub(self):
+        rospy.Subscriber("/power_supply", PowerSupply, self.callback)
+    
+    def shutDown(self):
+        id = str(uuid.uuid4())
+        self.df.to_csv(self.path+'/data/'+id+".csv")
+        rospy.signal_shutdown("Reached 5 minutes")
+
+
+if __name__ == '__main__':
+    rospy.init_node('dataProcessing', anonymous=True, disable_signals=True)
+    node = processor()
+    rospy.spin()
